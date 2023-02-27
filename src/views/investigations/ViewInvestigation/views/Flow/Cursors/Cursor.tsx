154f0cda7842ea5a917a43f87a5d6a2476fafed4
Alex <alex@shopsafe.uk>
2023-02-27 23:16:44 +0000

    Removed logs

--- src/views/investigations/ViewInvestigation/views/Flow/Cursors/Cursor.tsx
+++ src/views/investigations/ViewInvestigation/views/Flow/Cursors/Cursor.tsx
@@ -11,7 +11,6 @@ interface CursorProps {
 
 const Cursor = React.memo(({ cursor, color, name }: CursorProps) => {
   const { x, y } = cursor;
-  console.log(cursor);
   return (
     <div
       style={{
