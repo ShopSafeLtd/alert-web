8776227ddb366658cd52f5c0b16ab72401760408
Alex <alex@shopsafe.uk>
2023-05-02 14:02:51 +0100

    Redirect from staging to live

--- src/navigation/auth-views/authentication/login-1/index.tsx
+++ src/navigation/auth-views/authentication/login-1/index.tsx
@@ -6,6 +6,17 @@ import type { Props } from '../../components/LoginForm';
 import { LoginForm } from '../../components/LoginForm';
 import Loading from '../../../../components/loading';
 
+const getRedirect = () => {
+  const location = window.location.origin;
+  if (location.includes('localhost')) {
+    return location;
+  }
+  if (location.includes('staging')) {
+    return 'https://app.shopsafealert.co.uk/';
+  }
+  return location;
+};
+
 const LoginOne = (props: Props): JSX.Element => {
   const theme = useStoreState((state) => state.theme.currentTheme);
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
@@ -15,14 +26,18 @@ const LoginOne = (props: Props): JSX.Element => {
         ? 'linear-gradient(to right, #cb2d3e, #ef473b)'
         : 'linear-gradient(to right, #cb2d3e, #ef473a)',
   };
+
   useEffect(() => {
     if (!isAuthenticated && !isLoading) {
       if (localStorage.getItem('logo')?.endsWith('.webp')) {
         loginWithRedirect({
           'ext-logo': localStorage.getItem('logo'),
+          redirectUri: getRedirect(),
         });
       } else {
-        loginWithRedirect();
+        loginWithRedirect({
+          redirectUri: getRedirect(),
+        });
       }
     }
   }, []);
