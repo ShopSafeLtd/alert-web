2f0918ef338d0299fb48baf726cfdd36d9d1b3f5
Alex <alex@shopsafe.uk>
2023-06-02 17:48:28 +0100

    Edit default width on article image

--- src/utils/add-default-to-article.ts
+++ src/utils/add-default-to-article.ts
@@ -4,7 +4,7 @@ function extracted(html: string | undefined): string {
   // eslint-disable-next-line no-restricted-syntax, @typescript-eslint/naming-convention,no-underscore-dangle
   for (const img_ of imgs) {
     if (!img_.hasAttribute('width')) {
-      img_.setAttribute('width', '300');
+      img_.setAttribute('width', '600');
     }
     if (!img_.hasAttribute('height')) {
       img_.setAttribute('height', 'auto');
