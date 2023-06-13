3c924736ecc93cce6aec4f938e59509ee11f5155
Alex <alex@shopsafe.uk>
2023-06-13 11:39:43 +0100

    Build fixes

--- src/views/mg11/GenerateMg11/index.tsx
+++ src/views/mg11/GenerateMg11/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { processText } from '../../../utils/generate-text';
+import processText from '../../../utils/generate-text';
 
 interface Mg11Data {
   name: string;
