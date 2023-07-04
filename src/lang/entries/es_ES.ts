df00d90a17ec588a10a280ec36490d1bc28b21cd
Alex <alex@shopsafe.uk>
2023-07-04 16:36:24 +0100

    Removed lang from ignore

---
+++ src/lang/entries/es_ES.ts
@@ -0,0 +1,11 @@
+import antdEsES from 'antd/es/locale/es_ES';
+import esMsg from '../locales/es_ES.json';
+
+const EsLang = {
+  antd: antdEsES,
+  locale: 'es-ES',
+  messages: {
+    ...esMsg,
+  },
+};
+export default EsLang;
---
+++ src/lang/entries/it_IT.ts
@@ -0,0 +1,11 @@
+import antdItIT from 'antd/es/locale/it_IT';
+import itMSg from '../locales/it_IT.json';
+
+const ItLang = {
+  antd: antdItIT,
+  locale: 'en-US',
+  messages: {
+    ...itMSg,
+  },
+};
+export default ItLang;
