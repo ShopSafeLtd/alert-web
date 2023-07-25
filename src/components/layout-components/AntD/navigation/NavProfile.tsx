57d1fc37d9055dd447ca411a690543cdb6f038fd
Alex <alex@shopsafe.uk>
2023-07-25 11:53:48 +0100

    Added in initial languages
    Translations not added yet

--- src/components/layout-components/AntD/navigation/NavProfile.tsx
+++ src/components/layout-components/AntD/navigation/NavProfile.tsx
@@ -249,6 +249,48 @@ export const NavProfile = () => {
                         id: 'RVX8BX',
                       }),
                     },
+                    {
+                      value: 'nl',
+                      label: intl.formatMessage({
+                        defaultMessage: 'Dutch 🇳🇱',
+                        id: 'kVa3St',
+                      }),
+                    },
+                    {
+                      value: 'rbe',
+                      label: intl.formatMessage({
+                        defaultMessage: 'Belgian 🇧🇪',
+                        id: '0P84j7',
+                      }),
+                    },
+                    {
+                      value: 'pt',
+                      label: intl.formatMessage({
+                        defaultMessage: 'Portuguese 🇵🇹',
+                        id: '7gRkSQ',
+                      }),
+                    },
+                    {
+                      value: 'sv',
+                      label: intl.formatMessage({
+                        defaultMessage: 'Swedish 🇸🇪',
+                        id: 'Sda1tF',
+                      }),
+                    },
+                    {
+                      value: 'pl',
+                      label: intl.formatMessage({
+                        defaultMessage: 'Polish 🇵🇱',
+                        id: 'L66cYZ',
+                      }),
+                    },
+                    {
+                      value: 'fi',
+                      label: intl.formatMessage({
+                        defaultMessage: 'Finnish 🇫🇮',
+                        id: 'TpJCED',
+                      }),
+                    },
                   ]}
                 />
               </Row>
