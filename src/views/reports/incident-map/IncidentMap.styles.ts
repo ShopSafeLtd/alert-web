cae69f74947e284cd36576d99f257c3f0b7d6544
Will Garrod <will@shopsafe.uk>
2024-04-05 20:26:20 +0100

    Added police areas and more mapping settings

--- src/views/reports/incident-map/IncidentMap.styles.ts
+++ src/views/reports/incident-map/IncidentMap.styles.ts
@@ -17,6 +17,20 @@ const useStyles = createUseStyles({
   groupSelect: {
     width: 200,
   },
+  policeHover: {
+    position: 'absolute',
+    margin: 8,
+    padding: 4,
+    background: 'rgba(0, 0, 0, 0.8)',
+    color: '#fff',
+    maxWidth: 300,
+    fontSize: 10,
+    zIndex: 9,
+    pointerEvents: 'none',
+  },
+  mapContainer: {
+    position: 'relative',
+  },
 });
 
 export default useStyles;
