385677a6652acaa3bed5d8ef39d4f876dc618795
Alex <alex@shopsafe.uk>
2024-09-03 18:20:03 +0100

    Merged in bugfix/mysafety-import (pull request #464)

--- src/views/settings/data-import/mysafety/useMySafety.ts
+++ src/views/settings/data-import/mysafety/useMySafety.ts
@@ -1,6 +1,7 @@
 import type { MySafetyCSVData } from '#/components/form-components/MySafetyCSV/MySafetyCSV.types';
+import type { FormInstance } from 'antd';
 
-import { notification } from 'antd';
+import { Form, notification } from 'antd';
 import { useMySafetyImportDataMutation } from 'graphql/imports/__generated__/mysafety-import.generated';
 import { useState } from 'react';
 import { useStoreState } from 'state';
@@ -11,6 +12,7 @@ interface FormData {
 }
 
 interface Return {
+  form: FormInstance<FormData>;
   onSubmit: (data: FormData) => void;
   saving: boolean;
 }
@@ -18,7 +20,7 @@ interface Return {
 const useMySafety = (): Return => {
   const schemeId = useStoreState((state) => state.scheme.id);
   const [saving, setSaving] = useState(false);
-
+  const [form] = Form.useForm<FormData>();
   const [importData] = useMySafetyImportDataMutation({
     onCompleted: () => {
       notification.success({
@@ -39,24 +41,30 @@ const useMySafety = (): Return => {
   const onSubmit = async ({ groups, mySafety }: FormData) => {
     try {
       setSaving(true);
+
       await importData({
         variables: {
           data: {
             groups: groups.map((id) => ({ id })) || [],
-            incidents: mySafety.map((item) => ({
-              actualValue: item.actualValue,
-              createdByName: item.createdByName,
-              crimeReferenceNumber: item.crimeReferenceNumber,
-              crimeType: item.crimeType,
-              dateOccurred: item.dateOccurred,
-              description: item.description,
-              emergencyServicesAttend: item.emergencyServicesAttend,
-              estimatedValue: item.estimatedValue,
-              incidentID: item.incidentID,
-              site: item.site,
-              specificArea: item.specificArea,
-              wereWeaponsUsed: item.wereWeaponsUsed,
-            })),
+            incidents: mySafety
+              .filter(
+                // filter where actualValue isn't NaN
+                (item) => !Number.isNaN(item.actualValue)
+              )
+              .map((item) => ({
+                actualValue: item.actualValue,
+                createdByName: item.createdByName,
+                crimeReferenceNumber: item.crimeReferenceNumber,
+                crimeType: item.crimeType,
+                dateOccurred: item.dateOccurred,
+                description: item.description,
+                emergencyServicesAttend: item.emergencyServicesAttend,
+                estimatedValue: item.estimatedValue,
+                incidentID: item.incidentID,
+                site: item.site,
+                specificArea: item.specificArea,
+                wereWeaponsUsed: item.wereWeaponsUsed,
+              })),
             scheme: {
               id: schemeId,
             },
@@ -70,6 +78,7 @@ const useMySafety = (): Return => {
   };
 
   return {
+    form,
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     onSubmit,
     saving,
