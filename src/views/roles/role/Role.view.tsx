1e21c7298a7aa936cba6db5b917b6ee1d0e708c2
Alex <alex@shopsafe.uk>
2024-10-26 00:12:51 +0100

    Added approval check to role

--- src/views/roles/role/Role.view.tsx
+++ src/views/roles/role/Role.view.tsx
@@ -150,6 +150,9 @@ const RoleView = ({
     DATA_IMPORT: formatMessage({
       defaultMessage: 'Data Import',
     }),
+    DEM: formatMessage({
+      defaultMessage: 'DEM',
+    }),
     DOCUMENTS: formatMessage({
       defaultMessage: 'Documents',
     }),
@@ -222,7 +225,7 @@ const RoleView = ({
     <Form.Item
       label={labels[dataType]}
       labelAlign="left"
-      labelCol={{ span: 4 }}
+      labelCol={{ span: 7 }}
       name={dataType}
     >
       <Checkbox.Group
@@ -276,7 +279,7 @@ const RoleView = ({
             <Col span={18}>
               <Card loading={loading}>
                 <Row>
-                  <Col span={20}>
+                  <Col span={24}>
                     <Collapse defaultActiveKey={create ? '1' : undefined}>
                       <Collapse.Panel
                         header={formatMessage({
@@ -305,7 +308,7 @@ const RoleView = ({
                                     defaultMessage: 'Name',
                                   })}
                                   labelAlign="left"
-                                  labelCol={{ span: 4 }}
+                                  labelCol={{ span: 7 }}
                                   name="name"
                                   rules={[
                                     {
@@ -323,7 +326,7 @@ const RoleView = ({
                                     defaultMessage: 'Type',
                                   })}
                                   labelAlign="left"
-                                  labelCol={{ span: 4 }}
+                                  labelCol={{ span: 7 }}
                                   name="type"
                                   rules={[
                                     {
@@ -431,6 +434,17 @@ const RoleView = ({
                                 </Form.Item>
                               </>
                             )}
+                            <Form.Item
+                              label={formatMessage({
+                                defaultMessage: 'Can see/approve data',
+                              })}
+                              labelAlign="left"
+                              labelCol={{ span: 7 }}
+                              name="approvalAllowed"
+                              valuePropName="checked"
+                            >
+                              <Checkbox />
+                            </Form.Item>
                             {Object.values(PermissionModel)
                               .sort()
                               .map((key) => fields[key as DataType])}
