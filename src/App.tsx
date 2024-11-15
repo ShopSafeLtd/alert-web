e526fbab0e785081dc0789ed23e8248a6f57f968
Will Garrod <will@nvoyy.com>
2024-11-15 16:11:25 +0000

    Swapped to posthog

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { LoadScript } from '@react-google-maps/api';
 import { CaptureConsole, HttpClient } from '@sentry/integrations';
 import * as Sentry from '@sentry/react';
-import { configureScope, reactRouterV6Instrumentation } from '@sentry/react';
-import LogRocket from 'logrocket';
+import { reactRouterV6Instrumentation } from '@sentry/react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mixpanel from 'mixpanel-browser';
 import Views from 'navigation/router';
+import { PostHogProvider } from 'posthog-js/react';
 import React from 'react';
 import { ThemeSwitcherProvider } from 'react-css-theme-switcher/src';
 import 'react-grid-layout/css/styles.css';
@@ -21,6 +21,10 @@ import RouteWrapper from './navigation/utils/route-wrapper';
 import ApolloProvider from './providers/ApolloProvider';
 import { Store, ThemeConfig } from './state';
 
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
+
 const themes = {
   dark: '/css/dark-theme.css',
   light: '/css/light-theme.css',
@@ -29,15 +33,6 @@ const themes = {
 if (import.meta.env.PROD) {
   mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN);
   Sentry.init({
-    beforeSend(event) {
-      const logRocketSession = LogRocket.sessionURL;
-      if (logRocketSession !== null && event.extra) {
-        // eslint-disable-next-line no-param-reassign
-        event.extra.LogRocket = logRocketSession;
-        return event;
-      }
-      return event;
-    },
     dsn: import.meta.env.VITE_SENTRY_DSN,
 
     integrations: [
@@ -59,35 +54,33 @@ if (import.meta.env.PROD) {
     // Adjust for production
     tracesSampleRate: 0.4,
   });
-  LogRocket.init('ub3rsv/alert');
-
-  LogRocket.getSessionURL((sessionURL) => {
-    configureScope((scope) => {
-      scope.setExtra('sessionURL', sessionURL);
-    });
-  });
 }
 
 const App = (): JSX.Element => (
   <div className="App">
-    <ThemeSwitcherProvider
-      defaultTheme={ThemeConfig.currentTheme}
-      insertionPoint="styles-insertion-point"
-      themeMap={themes}
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
     >
-      <LoadScript
-        googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-        libraries={['visualization']}
+      <ThemeSwitcherProvider
+        defaultTheme={ThemeConfig.currentTheme}
+        insertionPoint="styles-insertion-point"
+        themeMap={themes}
       >
-        <Store>
-          <ApolloProvider>
-            <RouteWrapper title={undefined}>
-              <Views />
-            </RouteWrapper>
-          </ApolloProvider>
-        </Store>
-      </LoadScript>
-    </ThemeSwitcherProvider>
+        <LoadScript
+          googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+          libraries={['visualization']}
+        >
+          <Store>
+            <ApolloProvider>
+              <RouteWrapper title={undefined}>
+                <Views />
+              </RouteWrapper>
+            </ApolloProvider>
+          </Store>
+        </LoadScript>
+      </ThemeSwitcherProvider>
+    </PostHogProvider>
   </div>
 );
 
