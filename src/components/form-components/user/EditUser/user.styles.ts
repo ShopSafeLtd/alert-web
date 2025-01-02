c820e41863435dda9e6453a868407e44ffb8aa2c
ying <ying.luo@nvoyy.com>
2025-01-02 16:16:17 +0000

    updated user role

---
+++ src/components/form-components/user/EditUser/user.styles.ts
@@ -0,0 +1,15 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  switch: {
+    marginLeft: 10,
+    marginTop: -22,
+  },
+  switchItem: {
+    flexDirection: 'row',
+    justifyItems: 'flex-start',
+    marginBottom: 0,
+  },
+});
+
+export default useStyles;
