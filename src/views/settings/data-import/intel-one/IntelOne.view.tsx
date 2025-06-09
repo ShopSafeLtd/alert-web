5c16a3c0b044592d193665d975bda2fb35b14063
Will Garrod <will@shopsafe.uk>
2025-06-09 14:29:54 +0100

    Removes group selection from IntelOne import
    Removes the group selection component from the IntelOne data import view.
    The data import process no longer requires specifying groups, simplifying the user interface and workflow.

--- src/views/settings/data-import/intel-one/IntelOne.view.tsx
+++ src/views/settings/data-import/intel-one/IntelOne.view.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Button, Card, Form, Typography } from 'antd';
-import { FormattedMessage, useIntl } from 'react-intl';
-import GroupsSelect from '#/components/form-components/GroupsSelect/GroupsSelect.view';
 import IntelOneCSV from '#/components/form-components/IntelOneCSV/IntelOneCSV';
+import { Button, Card, Form, Typography } from 'antd';
+import React from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+
 import useIntelOne from './useIntelOne';
 
 const MySafety = () => {
   const intl = useIntl();
-  const { saving, onSubmit } = useIntelOne();
+  const { onSubmit, saving } = useIntelOne();
   return (
     <div style={{ padding: 20 }}>
       <Card>
@@ -17,37 +17,25 @@ const MySafety = () => {
         <Typography.Text>
           <FormattedMessage defaultMessage="Select a csv file from Intel One to import it." />
         </Typography.Text>
-        <Form onFinish={onSubmit} layout="vertical">
+        <Form layout="vertical" onFinish={onSubmit}>
           <Form.Item
             label={intl.formatMessage({
               defaultMessage: 'CSV Data',
             })}
+            name="intelOne"
+            required
             tooltip={intl.formatMessage({
               defaultMessage: 'Upload the CSV file from Intel One.',
             })}
-            name="intelOne"
-            required
           >
             <IntelOneCSV />
           </Form.Item>
-          <Form.Item
-            label={intl.formatMessage({
-              defaultMessage: 'Groups',
-            })}
-            tooltip={intl.formatMessage({
-              defaultMessage: 'Select the groups to import the data into.',
-            })}
-            name="groups"
-            required
-          >
-            <GroupsSelect mode="multiple" style={{ width: 250 }} />
-          </Form.Item>
           <Form.Item>
             <Button
-              htmlType="submit"
-              style={{ marginTop: 20 }}
-              loading={saving}
               disabled={saving}
+              htmlType="submit"
+              loading={saving}
+              style={{ marginTop: 20 }}
             >
               <FormattedMessage defaultMessage="Import Data" />
             </Button>
